add360f49f7ceed506dfbbe7c968de1e8258306a
monithavinod <monithavinod@gmail.com>
2026-01-24 12:53:58 +0530

    Update prog.c

--- Grade VI/B1/ Gas Detector/prog.c
+++ Grade VI/B1/ Gas Detector/prog.c
@@ -1,1 +1,26 @@
+// Gas Leakage Detection System using MQ-2 Sensor
+
+int gasSensorPin = A0;
+int buzzerPin = 8;
+int gasValue = 0;
+int threshold = 400;   // Adjust after testing
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  gasValue = analogRead(gasSensorPin);
+  Serial.print("Gas Sensor Value: ");
+  Serial.println(gasValue);
+
+  if (gasValue > threshold) {
+    digitalWrite(buzzerPin, HIGH);   // Gas detected
+  } else {
+    digitalWrite(buzzerPin, LOW);    // Normal condition
+  }
+
+  delay(500);
+}
 
