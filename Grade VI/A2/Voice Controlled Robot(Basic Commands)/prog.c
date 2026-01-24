3435d6111289164d88c315a403896468ded36b41
monithavinod <monithavinod@gmail.com>
2026-01-24 12:41:46 +0530

    Update prog.c

--- Grade VI/A2/Voice Controlled Robot(Basic Commands)/prog.c
+++ Grade VI/A2/Voice Controlled Robot(Basic Commands)/prog.c
@@ -1,1 +1,80 @@
+#include <SoftwareSerial.h>
+
+// Bluetooth pins
+SoftwareSerial bt(2, 3); // RX, TX
+
+// Motor driver pins
+#define IN1 5
+#define IN2 6
+#define IN3 9
+#define IN4 10
+
+char command;
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  Serial.begin(9600);
+  bt.begin(9600);
+
+  stopRobot();
+}
+
+void loop() {
+  if (bt.available()) {
+    command = bt.read();
+    Serial.println(command);
+
+    if (command == 'F') {
+      moveForward();
+    } else if (command == 'B') {
+      moveBackward();
+    } else if (command == 'L') {
+      turnLeft();
+    } else if (command == 'R') {
+      turnRight();
+    } else if (command == 'S') {
+      stopRobot();
+    }
+  }
+}
+
+// Movement functions
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopRobot() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
 
